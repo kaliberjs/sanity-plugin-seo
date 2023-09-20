5601b5dc66dfe075c67de9f7d981ee58437b721b
EECOLOR <ewestra@kaliber.net>
2023-09-20 17:56:29 +0200

    Schema as const instead of function

--- src/sanityPluginSeo.js
+++ src/sanityPluginSeo.js
@@ -1,5 +1,5 @@
 import { definePlugin } from 'sanity'
-import { buildSchema } from './schema'
+import { schema } from './schema'
 
 export { SeoAnalysis } from './components/SeoAnalysis'
 export { typeHasSeo } from './typeHasSeo'
@@ -8,7 +8,7 @@ export const sanityPluginSeo = definePlugin(
   () => ({
     name: 'sanity-plugin-seo',
     schema: {
-      types: (prev, context) => prev.concat(buildSchema())
+      types: (prev, context) => prev.concat(schema)
     },
   })
 )
--- src/schema.js
+++ src/schema.js
@@ -1,99 +1,97 @@
-export function buildSchema() {
-  return {
-    title: 'SEO',
-    name: 'seo',
-    type: 'object',
-    fields: [
-      {
-        title: 'Focus keyphrase',
-        name: 'keyphrase',
-        type: 'string',
+export const schema = {
+  title: 'SEO',
+  name: 'seo',
+  type: 'object',
+  fields: [
+    {
+      title: 'Focus keyphrase',
+      name: 'keyphrase',
+      type: 'string',
+    },
+    {
+      title: 'Synonyms',
+      name: 'synonyms',
+      type: 'string',
+    },
+    {
+      title: 'Mark as cornerstone content',
+      name: 'cornerstone',
+      type: 'boolean',
+    },
+    {
+      title: 'SEO title',
+      name: 'title',
+      type: 'string',
+    },
+    {
+      title: 'Meta description',
+      name: 'description',
+      type: 'text',
+      validation: Rule => Rule.max(156).warning('Try to limit the meta description to 156 characters.')
+    },
+    {
+      title: 'Social',
+      name: 'social',
+      type: 'object',
+      options: {
+        collapsible: true,
+        collapsed: true
       },
-      {
-        title: 'Synonyms',
-        name: 'synonyms',
-        type: 'string',
-      },
-      {
-        title: 'Mark as cornerstone content',
-        name: 'cornerstone',
-        type: 'boolean',
-      },
-      {
-        title: 'SEO title',
-        name: 'title',
-        type: 'string',
-      },
-      {
-        title: 'Meta description',
-        name: 'description',
-        type: 'text',
-        validation: Rule => Rule.max(156).warning('Try to limit the meta description to 156 characters.')
-      },
-      {
-        title: 'Social',
-        name: 'social',
-        type: 'object',
-        options: {
-          collapsible: true,
-          collapsed: true
+      fields: [
+        {
+          title: 'Title',
+          name: 'title',
+          type: 'string',
         },
-        fields: [
-          {
-            title: 'Title',
-            name: 'title',
-            type: 'string',
+        {
+          title: 'Description',
+          name: 'description',
+          type: 'text'
+        },
+        {
+          title: 'Share image',
+          name: 'shareImage',
+          type: 'image',
+          options: {
+            hotspot: true
           },
-          {
-            title: 'Description',
-            name: 'description',
-            type: 'text'
-          },
-          {
-            title: 'Share image',
-            name: 'shareImage',
-            type: 'image',
-            options: {
-              hotspot: true
+          fields: [
+            {
+              title: 'Alternative description',
+              name: 'alt',
+              type: 'string'
             },
-            fields: [
-              {
-                title: 'Alternative description',
-                name: 'alt',
-                type: 'string'
-              },
-            ]
-          }
-        ]
+          ]
+        }
+      ]
+    },
+    {
+      title: 'Advanced',
+      name: 'advanced',
+      type: 'object',
+      options: {
+        collapsible: true,
+        collapsed: true
       },
-      {
-        title: 'Advanced',
-        name: 'advanced',
-        type: 'object',
-        options: {
-          collapsible: true,
-          collapsed: true
+      fields: [
+        {
+          title: 'Canonical url',
+          name: 'canonicalUrl',
+          type: 'string',
         },
-        fields: [
-          {
-            title: 'Canonical url',
-            name: 'canonicalUrl',
-            type: 'string',
-          },
-          {
-            title: 'Allow indexation (index/noindex)',
-            name: 'allowIndex',
-            type: 'boolean',
-            initialValue: true
-          },
-          {
-            title: 'Allow following links (follow/nofollow)',
-            name: 'allowFollow',
-            type: 'boolean',
-            initialValue: true
-          },
-        ]
-      }
-    ]
-  }
+        {
+          title: 'Allow indexation (index/noindex)',
+          name: 'allowIndex',
+          type: 'boolean',
+          initialValue: true
+        },
+        {
+          title: 'Allow following links (follow/nofollow)',
+          name: 'allowFollow',
+          type: 'boolean',
+          initialValue: true
+        },
+      ]
+    }
+  ]
 }
